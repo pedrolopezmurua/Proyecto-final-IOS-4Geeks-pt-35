e40520b83a08f0aa34fad871f5efcbc203b3e326
CatalinaScheleff <catascheleff@gmail.com>
2023-05-13 10:08:02 -0400

    detalle

--- src/api/models.py
+++ src/api/models.py
@@ -8,12 +8,12 @@ class Proveedor(db.Model):
     _tablename_ = 'proveedor'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     rut = db.Column(db.String(20), nullable=False)
-    nombre = db.Column(db.String(50), nullable=False)
+    nombre = db.Column(db.String(100), nullable=False)
     apellido = db.Column(db.String(100), nullable=False)
     region = db.Column(db.String(100), nullable=False)
     comuna = db.Column(db.String(100), nullable=False)
     direccion = db.Column(db.String(200), nullable=False)
-    correo = db.Column(db.String(100), nullable=False)
+    correo = db.Column(db.String(100), unique=True, nullable=False)
     telefono = db.Column(db.Integer, nullable=False)
     red_social = db.Column(db.String(100), nullable=True)
     contraseña = db.Column(db.String(8), nullable=False)
@@ -48,15 +48,14 @@ class Categoria(db.Model):
 class Servicio(db.Model):
     _tablename_ = 'servicio'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    titulo = db.Column(db.String(100))
-    detalle = db.Column(db.String(500))
+    titulo = db.Column(db.String(200))
+    detalle = db.Column(db.String(1000))
     precio = db.Column(db.Integer, nullable=False)
     proveedor_id = db.Column(db.Integer, ForeignKey(
         'proveedor.id'), nullable=False)
     categoria_id = db.Column(db.Integer, ForeignKey(
         'categoria.id'), nullable=False)
-    region = db.Column(db.String(50))
-    cobertura_servicio = db.Column(db.String(200))
+    cobertura = db.Column(db.String(1000))
     estado = db.Column(db.Boolean, default=True)
     proveedor = db.relationship('Proveedor', backref='servicios')
     categoria = db.relationship('Categoria', backref='servicios')
@@ -69,8 +68,7 @@ class Servicio(db.Model):
             'precio': self.precio,
             'proveedor_id': self.proveedor_id,
             'categoria_id': self.categoria_id,
-            'region': self.region,
-            'cobertura_servicio': self.cobertura_servicio,
+            'cobertura': self.cobertura,
             'estado': self.estado,
             'proveedor': self.proveedor.serialize()
         }
