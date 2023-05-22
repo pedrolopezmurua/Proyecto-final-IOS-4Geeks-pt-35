710bbf3e9a206777c329c4a7256aa3f6405f1127
JorgeAymar <jorge.s.aymar@gmail.com>
2023-05-21 20:44:41 -0400

    my last

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,17 +1,7 @@
-//import react into the bundle
+// .index.js
+
 import React from "react";
-<<<<<<< HEAD
-import { createRoot } from "react-dom/client";
-=======
-import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom';
-
->>>>>>> 9e5bf8037ab457a9306b9ee8293ed725addfc8a3
-
-//include your index.scss file into the bundle
-import "../styles/index.css";
-
-//import your own components
+import ReactDOM, { createRoot } from 'react-dom';
 import Layout from "./layout";
 
 // Create root for concurrent mode
