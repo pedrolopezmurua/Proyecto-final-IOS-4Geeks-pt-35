b10eedbae474557cb68f02bd3d88f69e9d237c6d
JorgeAymar <jorge.s.aymar@gmail.com>
2023-05-30 19:32:00 -0400

    control de largos campos

--- src/front/js/pages/crearproveedor.js
+++ src/front/js/pages/crearproveedor.js
@@ -45,6 +45,12 @@ const CrearProveedor = () => {
             return;
         }
 
+        // Validar que la contraseña tenga más de 3 caracteres
+        if (formValues.contrasena.length < 4) {
+            showPopupError('La contraseña debe tener al menos 4 caracteres');
+            return;
+        }
+
         try {
             const response = await fetch('http://localhost:3001/api/proveedor', {
                 method: 'POST',
@@ -103,16 +109,16 @@ const CrearProveedor = () => {
                     <div className="form-group row mx-0">
                         <div className="col">
                             <label htmlFor="region">Region:</label>
-                            <input type="text" className="form-control" id="region" maxLength={100} style={{ width: '300px' }} onChange={handleChange} />
+                            <input type="text" className="form-control" id="region" style={{ width: '300px' }} minLength={3} maxLength={100} onChange={handleChange} />
                         </div>
                         <div className="col">
                             <label htmlFor="comuna">Comuna:</label>
-                            <input type="text" className="form-control" id="comuna" maxLength={100} style={{ width: '300px' }} onChange={handleChange} />
+                            <input type="text" className="form-control" id="comuna" style={{ width: '300px' }} minLength={3} maxLength={100} onChange={handleChange} />
                         </div>
                     </div>
                     <div className="form-group">
                         <label htmlFor="direccion">Dirección:</label>
-                        <input type="text" className="form-control" id="direccion" maxLength={200} style={{ width: '600px' }} onChange={handleChange} />
+                        <input type="text" className="form-control" id="direccion" style={{ width: '600px' }} minLength={3} maxLength={200} onChange={handleChange} />
                     </div>
                 </div>
             </div>
@@ -140,15 +146,15 @@ const CrearProveedor = () => {
                     <div className="form-group row mx-0">
                         <div className="col">
                             <label htmlFor="correo">Email:</label>
-                            <input type="email" className="form-control" id="correo" maxLength={100} style={{ width: '300px' }} onChange={handleChange} />
+                            <input type="email" className="form-control" id="correo" style={{ width: '300px' }} minLength={3} maxLength={100} onChange={handleChange} />
                         </div>
                         <div className="col">
                             <label htmlFor="contrasena">Ingrese Contraseña:</label>
-                            <input type="password" className="form-control" id="contrasena" maxLength={20} style={{ width: '300px' }} onChange={handleChange} />
+                            <input type="password" className="form-control" id="contrasena" style={{ width: '300px' }} minLength={3} maxLength={100} onChange={handleChange} />
                         </div>
                         <div className="col">
                             <label htmlFor="confirm-password">Confirmación de Contraseña:</label>
-                            <input type="password" className="form-control" id="confirm-password" maxLength={20} style={{ width: '300px' }} onChange={handleChange} />
+                            <input type="password" className="form-control" id="confirm-password" style={{ width: '300px' }} minLength={3} maxLength={20} onChange={handleChange} />
                         </div>
                     </div>
                 </div>
