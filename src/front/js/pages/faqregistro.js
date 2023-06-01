62f7fd0da6c29b0aa4022353b84321ba87849027
JorgeAymar <jorge.s.aymar@gmail.com>
2023-05-31 20:13:36 -0400

    pagina faq

---
+++ src/front/js/pages/faqregistro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function FaqRegistro() {
+    return () {
+        return (
+            <div>
+                <h1>Registro y Cuenta</h1>
+
+                <p>
+                    1. Registro y Cuenta: Para utilizar ciertas funciones del Sitio, es posible que deba registrarse y crear una cuenta. Usted es responsable de mantener la confidencialidad de su información de inicio de sesión y de todas las actividades que ocurran en su cuenta. Usted acepta proporcionar información precisa y actualizada al registrarse y mantenerla actualizada en todo momento.
+                </p>
+            </div>
+        );
+    }
+}
